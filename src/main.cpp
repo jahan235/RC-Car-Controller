c83c7e8c42c17db039d0a7921f8d68c5242a33cd
jahan235 <jahanzaibamail@gmail.com>
2025-07-02 05:13:42 -0400

    Restructured code -Removed separate update functions for the gauges -Added prepare_display_buffer function to prepare the display with all updated values -Added various input variables to  be used in the future -Speed gauge needle now has positioning functionality

--- src/main.cpp
+++ src/main.cpp
@@ -75,46 +75,56 @@ const unsigned char gauge_cluster [] PROGMEM = {
 
 const int RPM_needle_anchor_coordinates[2] = {32, 32};    // Coordinates for RPM needle pivot (center of gauge)
 const int speed_needle_anchor_coordinates[2] = {96, 32};  // Coordinates for speed needle pivot (center of gauge)
+const int gear_indicator_coordinates[2] = {55, 40}; //Coordinates for gear indicator text
+const int gear_indicator_font_size = 3;
+
+char selected_gear = 'N';
+int engine_RPM = 0;
+int vehicle_speed = 0;
+int steering_angle = 0;
+int throttle_position = 0;
+int brake_position = 0;
+int clutch_position = 0;
 
 
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void setup_display() {
+void prepare_display_buffer() {
   display.setTextColor(SSD1306_WHITE); //Set text color to white
   display.setCursor(0, 0);
-  display.drawBitmap(0, 0, gauge_cluster, SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE); // Draw the gauge cluster bitmap
+  
+  //Display guage cluster background
+  display.drawBitmap(0, 0, gauge_cluster, SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE);
+  
+  //Display gear indicator text
+  display.setCursor(gear_indicator_coordinates[0], gear_indicator_coordinates[1]);
+  display.setTextSize(gear_indicator_font_size); // Set text size for gear indicator
+  display.print(selected_gear); // Display the selected gear
+
+  //Set RPM needle position
+  display.drawLine(
+    RPM_needle_anchor_coordinates[0],
+    RPM_needle_anchor_coordinates[1],
+    RPM_needle_anchor_coordinates[0] + round(20 * cos((180.0 - engine_RPM) * (PI / 180.0))),
+    RPM_needle_anchor_coordinates[1] - round(20 * sin((180.0 - engine_RPM) * (PI / 180.0))),
+    SSD1306_WHITE
+    );
+
+  //Set speed needle position
+  display.drawLine(
+    speed_needle_anchor_coordinates[0],
+    speed_needle_anchor_coordinates[1],
+    speed_needle_anchor_coordinates[0] + round(20 * cos((180.0 - vehicle_speed) * (PI / 180.0))),
+    speed_needle_anchor_coordinates[1] - round(20 * sin((180.0 - vehicle_speed) * (PI / 180.0))),
+    SSD1306_WHITE
+    );
 }
 
-void update_RPM_gauge(int new_RPM){
-      display.clearDisplay(); // Clear the display before drawing new content
-      setup_display();
-      display.drawLine(
-        RPM_needle_anchor_coordinates[0],
-        RPM_needle_anchor_coordinates[1],
-        RPM_needle_anchor_coordinates[0] + round(20 * cos((180.0 - new_RPM) * (PI / 180.0))),
-        RPM_needle_anchor_coordinates[1] - round(20 * sin((180.0 - new_RPM) * (PI / 180.0))),
-        SSD1306_WHITE
-      );
-      display.display(); // Update the display with new content
-}
-
-void update_speed_gauge(int new_speed){
-      display.clearDisplay(); // Clear the display before drawing new content
-      setup_display();
-      display.drawLine(
-        speed_needle_anchor_coordinates[0],
-        speed_needle_anchor_coordinates[1],
-        speed_needle_anchor_coordinates[0] + round(20 * cos((180.0 - new_speed) * (PI / 180.0))),
-        speed_needle_anchor_coordinates[1] - round(20 * sin((180.0 - new_speed) * (PI / 180.0))),
-        SSD1306_WHITE
-      );
-      display.display(); // Update the display with new content
-}
-
-void update_gauges(int new_RPM, int new_speed) {
-    update_RPM_gauge(new_RPM); // Update RPM gauge
-    update_speed_gauge(new_speed); // Update speed gauge
+void update_display(){
+  display.clearDisplay();
+  prepare_display_buffer();
+  display.display();
 }
 
 
@@ -128,20 +138,11 @@ void setup() {
 
   Serial.println(F("OLED initialized successfully!"));
 
-  // Clear the buffer
-  display.clearDisplay();
-
-  // Set text size, color, and cursor position
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-
-  display.drawBitmap(0, 0, gauge_cluster, SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE);
-  // Display the content of the buffer on the OLED screen
-  display.display();
+  //Initialize the display
+  update_display();
 }
 
 
-int engine_RPM = 0;
 void loop() {
 
     if(Serial.available()){
@@ -150,9 +151,8 @@ void loop() {
       engine_RPM = map(RPM_input, 0, 2000, 0, 180); //Map RPM input to angle (0-180 degrees)
       Serial.print(F("Received RPM: "));
       Serial.println(RPM_input);
-      update_RPM_gauge(engine_RPM);  // Update the RPM gauge with the new value
-      Serial.println(F("Updated RPM needle on display."));
+      Serial.println(F("Updated RPM needle variable!"));
     }
 
-
+    update_display();
 }
