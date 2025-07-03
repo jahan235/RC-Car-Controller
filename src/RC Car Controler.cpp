88a579f8de221f621d764cd0422bac21636b416d
jahan235 <jahanzaibamail@gmail.com>
2025-07-03 02:07:17 -0400

    Added UI elements -Added turn signal indicators which operate using new  blinker_clock() function -Blinker state is tracked with new variable called blinker_mode: 0=off, 1=left, 2=right, 3=hazard -Added headlight state indicator -Added connection indicator which will be used to  display connection status with car in the future -prepare_display_buffer() function handles all indicator positioning and visibility

--- src/RC Car Controler.cpp
+++ src/RC Car Controler.cpp
@@ -5,6 +5,10 @@
 const int RPM_needle_anchor_coordinates[2] = {32, 32};    // Coordinates for RPM needle pivot (center of gauge)
 const int speed_needle_anchor_coordinates[2] = {96, 32};  // Coordinates for speed needle pivot (center of gauge)
 const int gear_indicator_coordinates[2] = {55, 40}; //Coordinates for gear indicator text
+const int headlight_indicator_coordinates[2] = {30, 50}; //Coordinates for headlight indicator icon
+const int left_turn_signal_indicator_coordinates[2] = {30, 40}; //Coordinates for left turn signal indicator icon
+const int right_turn_signal_indicator_coordinates[2] = {77, 40}; //Coordinates for right turn signal indicator icon
+const int connection_indicator_coordinates[2] = {77, 50}; //Coordinates for connection indicator icon
 const int gear_indicator_font_size = 3;
 
 char selected_gear = 'N';
@@ -14,10 +18,28 @@ int steering_angle = 0;
 int throttle_position = 0;
 int brake_position = 0;
 int clutch_position = 0;
+int headlight_state = 1;
+int turn_signal_mode = 3; //0 = off, 1 = left, 2 = right, 3 = hazard
+
+int turn_signal_cycle_duration = 500;  //Duration of on/off cycle for turn signals
 
 
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+//Function to handle blinker flashing.  Returns true while blinker should be active, false otherwise.
+bool blinker_clock(){
+	static long last_blink_time = 0;
+	static bool blinker_state = false;
+
+	if(millis() - last_blink_time >= turn_signal_cycle_duration){
+		blinker_state = !blinker_state; //Toggle blinker
+		last_blink_time = millis(); //Update last blink time
+	}
+
+	return blinker_state;
+}
+
 
 void prepare_display_buffer() {
   display.setTextColor(SSD1306_WHITE); //Set text color to white
@@ -30,6 +52,23 @@ void prepare_display_buffer() {
   display.setCursor(gear_indicator_coordinates[0], gear_indicator_coordinates[1]);
   display.setTextSize(gear_indicator_font_size); // Set text size for gear indicator
   display.print(selected_gear); // Display the selected gear
+
+  //Display headlight icon if headlights are on
+  if(headlight_state){
+	display.drawBitmap(headlight_indicator_coordinates[0], headlight_indicator_coordinates[1], Sprites::headlight_indicator, 15, 10, SSD1306_WHITE);
+  }
+
+  //Display turn signal indicators
+  if(blinker_clock()){
+	//Left turn signal indicator is only active if turn signal mode is 1 or 3 (hazard)
+	if(turn_signal_mode == 1 || turn_signal_mode == 3){
+		display.drawBitmap(left_turn_signal_indicator_coordinates[0], left_turn_signal_indicator_coordinates[1], Sprites::left_turn_signal_indicator, 15, 10, SSD1306_WHITE);
+  	}
+	//Right turn signal indicator is only active if turn signal mode is 2 or 3 (hazard)
+	if(turn_signal_mode == 2 || turn_signal_mode == 3){
+		display.drawBitmap(right_turn_signal_indicator_coordinates[0], right_turn_signal_indicator_coordinates[1], Sprites::right_turn_signal_indicator, 15, 10, SSD1306_WHITE);
+	}
+}
 
   //Set RPM needle position
   display.drawLine(
@@ -48,7 +87,13 @@ void prepare_display_buffer() {
     speed_needle_anchor_coordinates[1] - round(20 * sin((180.0 - vehicle_speed) * (PI / 180.0))),
     SSD1306_WHITE
     );
+
+  //Display connection indicator
+  if(true){
+	display.drawBitmap(connection_indicator_coordinates[0], connection_indicator_coordinates[1], Sprites::connection_indicator, 15, 10, SSD1306_WHITE);
+  }
 }
+
 
 void update_display(){
   display.clearDisplay();
